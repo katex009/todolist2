10730b2784838743271af844cb9b876287431a3a
kate <kate09030506@gmail.com>
2025-01-11 16:38:48 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    import AppHeader from './components/AppHeader.vue';
+    import ListItem from './components/ListItem.vue';
+</script>
+
+<template>
+    <AppHeader />
+
+</template>
+
+<style scoped>
+</style>
